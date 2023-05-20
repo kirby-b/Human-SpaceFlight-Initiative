03a70651523a6c859bf085547f95b5f19d09e9c3
kirby-b <kirby.brodie66@gmail.com>
2023-05-20 10:24:50 -0400

    beginning eva

--- MissionControl.java
+++ MissionControl.java
@@ -3,6 +3,8 @@ import java.util.*;
 class MissionControl{
   boolean flight;//is the rocket in flight or in free fall
   boolean para = false; // Boolean to tell if parachute is open yet
+  int evaChoice = 0; // Uses a 0, 1, or 2 to determine if eva should happen. 0 is default and means it hasnt asked
+  int evaTime = 0;
   Random rand = new Random();
   int upperbound = 100; //making a random number
   int rando = 0 ; // For holding random numbers
@@ -12,6 +14,7 @@ class MissionControl{
   long velocity;//how fast the rocket is going
   double airtime;//how long its been in air
   double fallingtime;//how long has it been falling
+  double distanceFromRocket; // Distance from rocket during EVA
   double Missiontime; // How long the mission has been going
   int days = 0;
   int hours = 0;// Minutes, hours, and days in air
@@ -19,9 +22,12 @@ class MissionControl{
   int fdays = 0;
   int fhours = 0; // Minutes, hours, and days falling
   int fminutes = 0;
+  //int evaMinutes = 0;
+  //int evaSeconds = 0;
   String heightString;
   String velocityString;
   String[] countDown = {"5","4","3","2","1","LIFTOFF"};  //what is says on countdown
+  String[] items = new String[10]; // List for items found in eva with a max of 20
   Rocket rocket = new Rocket(); // able to call methods from rocket file
   Launch launch=new Launch();//able to call methods from Launch file
 
@@ -51,8 +57,9 @@ class MissionControl{
       s.close();
   }
   public void displayInfo(){
-      // displays information about the rocket
-      
+      /* displays information about the rocket(in flight)(in free fall)(how much gas)
+      (the speed)(the hieght)(the distance)
+      */    
       
       velocity = velocity + (long)launch.Speed("speed");
       while(true){
@@ -96,6 +103,36 @@ class MissionControl{
             }
           }
               // Makes rocket fall at -9.81 meters per second
+              
+              /*if (evaChoice == 0){
+                Scanner s = new Scanner(System.in);
+                while (true){
+                  try{
+                  System.out.println("Would you like to preform a 5 minute EVA");
+                  evaChoice = s.nextInt();
+                  if(evaChoice != 1 && evaChoice != 2){
+                    System.out.println("Invalid input!");
+                  }
+                  else{
+                    s.close();
+                    break;
+                  }
+                  }
+                  catch(InputMismatchException e){
+                    System.out.println("Invalid input!");
+                  }
+                }
+              }
+              if (evaChoice == 1){
+                eva();
+              }
+              //Activates EVA if the users chooses to
+            }
+          }
+          if (evaChoice == 2 && distanceFromRocket > 0){
+            System.out.println("Astronaut is returning to ship");
+            distanceFromRocket--;
+          }*/
 
           flight = rocket.inFlight(velocity);
           flight = rocket.freeFall(velocity);
@@ -142,7 +179,8 @@ class MissionControl{
               if(flight == true){
                   Thread.sleep(999);
                   System.out.print("The rocket is in flight.\n"+"The rocket is "+height+" meter(s) above the ground\n"+
-                  "The rocket has this much fuel left "+fuel+"\nThe rocket is going "+velocity+" meter(s) per seconds.\n");
+                  "The Astronuats are "+distanceFromRocket+" meter(s) from the rocket\n"+"The rocket has this much fuel left "+fuel
+                  +"\nThe rocket is going "+velocity+" meter(s) per seconds.\n");
                   if(airtime<3600){
                   System.out.println("Rocket has been flying for "  + minutes + " minute(s), "  + seconds + " second(s).");
                   }
@@ -161,7 +199,8 @@ class MissionControl{
                   Thread.sleep(999);
                   
                   System.out.print("The rocket is falling.\n"+"The rocket is ~"+height+" meter(s) above the ground\n"+
-                  "The rocket has this much fuel left "+fuel+"\nThe rocket is falling at ~"+Math.abs(velocity)+" meters per second\n");
+                  "The Astronuats are "+distanceFromRocket+" meter(s) from the rocket\n"+"The rocket has this much fuel left "+fuel
+                  +"\nThe rocket is falling at ~"+Math.abs(velocity)+" meters per second\n");
                   if(fallingtime<3600){
                   System.out.println("Rocket has been falling for "  + fminutes + " minute(s), "  + fseconds + " second(s).");
                   }
@@ -181,15 +220,58 @@ class MissionControl{
         }
   }
   public void count(){
-    for(int i=0;i<countDown.length;i++){/*steps through every index of array */ 
-      try {
-          Thread.sleep(1000);// waits 1 second
-      } 
-      catch (InterruptedException e) {
-          // catch  error
-          e.printStackTrace();
-      }
-      System.out.println(countDown[i]);  //prints the countdown
+  for(int i=0;i<countDown.length;i++){/*steps through every index of array */ {
+    try {
+        Thread.sleep(1000);// waits 1 second
+    } catch (InterruptedException e) {
+        // catch  error
+        e.printStackTrace();
     }
+    System.out.println(countDown[i]);}  //prints the countdown
+  }
+  /*public void eva(){
+    rando = rand.nextInt(upperbound);
+    System.out.println("Astronaut is preforming an EVA");
+        // Do the math for eva time
+        evaSeconds = evaTime;
+        if (evaSeconds > 60){
+          evaMinutes = evaSeconds / 60;
+          evaSeconds = evaSeconds % 60;
+        }
+        System.out.println("Rocket has been flying for "  + evaMinutes + " minute(s), "  + evaSeconds + " second(s).");
+    if (itemsFound < 10){
+      if (rando <= 10 && rando > 0){
+        items[itemsFound] = "Space Rock Sample";
+        itemsFound++;
+      }
+      else if (rando <= 21 && rando > 25){
+        items[itemsFound] = "Small Scrap Chunk";
+        itemsFound++;
+      }
+      else if (rando == 50){
+        items[itemsFound] = "Scrap Chunk";
+        itemsFound++;
+      }
+      else if (rando == 70){
+        items[itemsFound] = "Small Mineral Sample";
+        itemsFound++;
+      }
+      //Adds items to astronauts inventory
+    }
+    else{
+      System.out.println("The Astronaut's bag is full so they are just practicing swimming through space.");
+    }
+    System.out.println("They have found:");
+    for (int i = 0; i < items.length; i++){
+      if (!items[i].equals("")){
+        System.out.println(items[i] + "\n");
+      }
+      //Displays findings
+    }
+    distanceFromRocket += 0.5;
+    evaTime++;
+    if (evaTime >= 300){
+      evaChoice = 2;
+    }*/
   }
 }
